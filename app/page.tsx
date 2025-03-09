2f982af3097ced6e990dc7d991547519000e5b12
amir-mc <amirfooladi93@gmail.com>
2025-03-09 13:13:32 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
+import Feed from "@/components/feed"
+import Share from "@/components/share"
+import Link from "next/link"
 
-import Imagetoolkit from "@/components/image";
+
 
 const Homepage = () => {
 
   return (
-     <div className='relative w-[600px] h-[600px]'>
-    
-   <Imagetoolkit path="/general/postImg.jpeg" width={300} height={300} alt="Alt text"/>
-   
-   </div>
+    <div>
+      <div className="pt-4 px-4 flex justify-between text-gray-500 font-bold border-d-2 border-gray-500">
+        <Link href="/" className="pb-3 flex items-center border-b-4 border-b-blue-700">For you</Link>
+        <Link href="/" className="pb-3 flex items-center ">ALL</Link>
+        <Link href="/" className="pb-3 flex items-center ">ALL</Link>
+        <Link href="/" className="pb-3 flex items-center ">ALL</Link>
+        <Link href="/" className="pb-3 flex items-center ">ALL</Link>
+        <Link href="/" className="pb-3 flex items-center ">ALL</Link>
+      </div>
+<Share/>
+<Feed/>
+
+    </div>
+     
   )
 }
 
