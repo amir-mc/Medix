76e7bb9ecfc6cf37578f64ea14bd79446c6854d4
amir-mc <amirfooladi93@gmail.com>
2025-03-16 14:20:42 +0000

    update

---
+++ components/imageEditor.tsx
@@ -0,0 +1,127 @@
+import {  X } from "lucide-react";
+import Image from "next/image";
+import { Dispatch, SetStateAction } from "react";
+
+const ImageEditor = ({
+    onClose,
+    PrviewImg,
+    setting,
+    setSetting,
+  }: {
+    onClose: () => void;
+    PrviewImg: string;
+    setting: {
+      type: "original" | "wide" | "square";
+      sensitive: boolean;
+    };
+    setSetting: React.Dispatch<
+      React.SetStateAction<{
+        type: "original" | "wide" | "square";
+        sensitive: boolean;
+      }>
+    >;
+  }) => {
+    const handleChangeSensitive = (sensitive: boolean) => {
+        setSetting((prev) => ({ ...prev, sensitive }));
+    };
+    const handleChangeType = (type: "original" | "wide" | "square") => {
+        setSetting((prev) => ({ ...prev, type }));
+    };
+    return (
+        <div className=" fixed inset-0 bg-black/80 flex items-center justify-center p-4">
+      <div className="bg-gray-900 text-white rounded-2xl shadow-lg p-6 w-full max-w-lg flex flex-col gap-6">
+          {/* TOP */}
+
+            <div className="flex items-center gap-8  justify-center">
+              <X 
+              onClick={onClose}
+              />
+              <h1 className="font-bold text-xl">Media Settings</h1>
+            </div>
+            <button className="py-2 px-4 rounded-full bg-white text-black font-bold" onClick={onClose}>
+              Save
+            </button>
+      
+          {/* IMAGE CONTAINER */}
+          <div className="w-[300px] h-[300px] flex items-center">
+            <Image
+              src={PrviewImg}
+              alt=""
+              width={600}
+              height={600}
+              className={`w-full ${
+                setting.type === "original"
+                  ? "h-full object-contain"
+                  : setting.type === "square"
+                  ? "aspect-square object-cover"
+                  : "aspect-video object-cover"
+              }`}
+            />
+          </div>
+          {/* SETTINGS */}
+          <div className="flex items-center justify-between text-sm">
+            <div className="flex items-center gap-8">
+              <div
+                className="flex items-center gap-2 cursor-pointer"
+                onClick={() => handleChangeType("original")}
+              >
+                <svg width={24} viewBox="0 0 24 24">
+                  <path
+                    className={
+                      setting.type === "original"
+                        ? "fill-iconBlue"
+                        : "fill-[#e7e9ea]"
+                    }
+                    d="M3 7.5C3 6.119 4.119 5 5.5 5h13C19.881 5 21 6.119 21 7.5v9c0 1.381-1.119 2.5-2.5 2.5h-13C4.119 19 3 17.881 3 16.5v-9zM5.5 7c-.276 0-.5.224-.5.5v9c0 .276.224.5.5.5h13c.276 0 .5-.224.5-.5v-9c0-.276-.224-.5-.5-.5h-13z"
+                  />
+                </svg>
+                Original
+              </div>
+              <div
+                className="flex items-center gap-2 cursor-pointer"
+                onClick={() => handleChangeType("wide")}
+              >
+                <svg width={24} viewBox="0 0 24 24">
+                  <path
+                    className={
+                      setting.type === "wide"
+                        ? "fill-iconBlue"
+                        : "fill-[#e7e9ea]"
+                    }
+                    d="M3 9.5C3 8.119 4.119 7 5.5 7h13C19.881 7 21 8.119 21 9.5v5c0 1.381-1.119 2.5-2.5 2.5h-13C4.119 17 3 15.881 3 14.5v-5zM5.5 9c-.276 0-.5.224-.5.5v5c0 .276.224.5.5.5h13c.276 0 .5-.224.5-.5v-5c0-.276-.224-.5-.5-.5h-13z"
+                  />
+                </svg>
+                Wide
+              </div>
+              <div
+                className="flex items-center gap-2 cursor-pointer"
+                onClick={() => handleChangeType("square")}
+              >
+                <svg width={24} viewBox="0 0 24 24">
+                  <path
+                    className={
+                      setting.type === "square"
+                        ? "fill-iconBlue"
+                        : "fill-[#e7e9ea]"
+                    }
+                    d="M3 5.5C3 4.119 4.119 3 5.5 3h13C19.881 3 21 4.119 21 5.5v13c0 1.381-1.119 2.5-2.5 2.5h-13C4.119 21 3 19.881 3 18.5v-13zM5.5 5c-.276 0-.5.224-.5.5v13c0 .276.224.5.5.5h13c.276 0 .5-.224.5-.5v-13c0-.276-.224-.5-.5-.5h-13z"
+                  />
+                </svg>
+                Square
+              </div>
+            </div>
+            <div
+              className={`cursor-pointer py-1 px-4 rounded-full text-black ${
+                setting.sensitive ? "bg-red-500" : "bg-white"
+              }`}
+              onClick={() => handleChangeSensitive(!setting.sensitive)}
+            >
+              Sensitive
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+ 
+export default ImageEditor;
--- components/share.tsx
+++ components/share.tsx
@@ -3,22 +3,43 @@ import { Calendar, Check, CheckCheck, CheckCircle, Gift, Image, List, LocateIcon
 import Imagetoolkit from "./image";
 import { useState } from "react";
 import { ShareAction } from "@/action/action";
+import  NextImage  from "next/image";
+import ImageEditor from "./imageEditor";
 
 
 const Share = () => {
     const  [media,setMedia]=useState<File | null>(null)
+    const  [setEditoropen,setidEditoropen]=useState(false)
+    const  [setting,setSetting]=useState<{
+        type:"original" | "wide" | "square"
+        sensitive:boolean   
+    }>({
+        type:"original",
+        sensitive:false
+    })
     const handleMedia=(e:React.ChangeEvent<HTMLInputElement>)=>{
         if(e.target.files && e.target.files[0]){
             setMedia(e.target.files[0])
         }
     }
+    const PrviewImg=media ? URL.createObjectURL(media):null
     return ( 
         <form className="p-4 flex gap-4" action={ShareAction}>
                 <div className="relative w-10 h-10 rounded-full overflow-hidden">
                     <Imagetoolkit path='general/userImg.jpeg' alt='profile' w={100} h={100} />
                 </div>
                 <div className="flex-1 flex flex-col gap-4">
-                    <input type="text" name="des" placeholder="Loking for somthing "  className="bg-transparent outline-none placeholder:text-gray-500"/>
+                    <input type="text" name="des" placeholder="Looking for something "  className="bg-transparent outline-none placeholder:text-gray-500"/>
+                {PrviewImg &&
+                    <div className="relative rounded-2xl overflow-hidden">
+                        <NextImage src={PrviewImg} alt="pre" width={600} height={600} />
+                        <div  className="absolute left-2 top-2 bg-teal-300 opacity-60 cursor-pointer  rounded-4xl text-black font-bold py-1 px-3 " onClick={()=>setidEditoropen(true)}>Edit</div>
+                    </div>
+                }
+                {
+                    setEditoropen && PrviewImg && <ImageEditor
+                     onClose={()=>setidEditoropen(false)} PrviewImg={PrviewImg} setting={setting} setSetting={setSetting}/>
+                }
                 <div className="flex items-center justify-between gap-4 flex-wrap">
                     <div className="flex gap-4 flex-wrap">
                         <input type="file" name="file" onChange={handleMedia} className="hidden" id="input" />
